c3c4f459b022cbce18e9f5285fadc5ce35f26470
서상원 <bboeric@Sangwons-MacBook-Pro.local>
2025-04-11 01:17:56 +0900

    test

---
+++ SOPT36_WEEK0/SOPT36_WEEK0/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SOPT36_WEEK0
+//
+//  Created by 서상원 on 4/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .green
+    }
+} 
+
